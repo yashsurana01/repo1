47611e8262dd6ea1ed6a35be519b0ba37bd68091
Yash Surana <yashsurana01@gmail.com>
2020-12-17 18:07:12 +0530

    added other msg to hello function in hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,12 @@
 class hello{
-public static void main(String args[]){
-Sytem.out.println("Hello");
-
-
-void hello(){}
+	public static void main(String args[]){
+	
+	Sytem.out.println("Hello from 2");
+	void hello(){}
 }
 }
+
+
+
+
+2
